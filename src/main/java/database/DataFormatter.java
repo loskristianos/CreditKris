77d0ebd12f70dcb27dd6dbb398dc140914148d4f
kris.hardy <kristian.hardy@gmail.com>
2023-12-21 22:30:14 +0000

    It works!

--- src/main/java/database/DataFormatter.java
+++ src/main/java/database/DataFormatter.java
@@ -1,26 +1,50 @@
 /*  Will take an object (any of the data objects) and prepare the appropriate
-*   SQL statement String to be passed to the read/write classes.
+*   SQL statement String to be passed to the read/write classes. I hope.
 */
 package database;
 
-public class DataFormatter {
-    String[] inputObject;
+import java.lang.reflect.Method;
 
-    public DataFormatter(String[] inputObject) {
+public class DataFormatter {
+
+    Object inputObject;
+    String tableName;
+
+    public DataFormatter(Object inputObject) {
         this.inputObject = inputObject;
+        this.tableName = inputObject.getClass().getPackageName();
     }
 
-    /*  takes the array of strings from the object (getAccountDetails, getCustomerDetails, etc.)
-        and convert them into a string of comma separated values to slot into the SQL query
-     */
-    public String extractValuesToString(){
+    public String extractValuesToString() throws Exception {
+        String[] inputArray;
+        Method method = null;
+        switch (tableName) {
+            case "accounts":
+                method = inputObject.getClass().getDeclaredMethod("getAccountDetails");
+                break;
+            case "transactions":
+                method = inputObject.getClass().getDeclaredMethod("getTransactionDetails");
+                break;
+            case "login":
+                method = inputObject.getClass().getDeclaredMethod("getLoginDetails");
+                break;
+            case "customer":
+                method = inputObject.getClass().getDeclaredMethod("getCustomerDetails");
+                break;
+        }
+        inputArray = (String[]) method.invoke(inputObject);
         StringBuilder x = new StringBuilder();
-        for (int i = 0; i < inputObject.length; i++) {
+        for (int i = 0; i < inputArray.length; i++) {
             x.append("'");
-            x.append(inputObject[i]);
+            x.append(inputArray[i]);
             x.append("'");
-            if (i != (inputObject.length-1)) x.append(",");
+            if (i != (inputArray.length - 1)) x.append(",");
         }
         return x.toString();
+    }
+
+    public String buildWriteQuery() throws Exception {
+        String writeQuery = "insert into " + tableName + " values(" + extractValuesToString() + ")";
+        return writeQuery;
     }
 }
--- src/test/java/database/DataFormatterTest.java
+++ src/test/java/database/DataFormatterTest.java
@@ -1,16 +1,37 @@
 package database;
 
+import accounts.ClientAccount;
 import org.junit.jupiter.api.Test;
+import transactions.TransferTransaction;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataFormatterTest {
 
-    String[] testData = {"18725", "walk", "kris", "hardy", "test"};
+    String[] testAccountData = {"123", "40-25-99", "12345678", "308.50", "500", "1"};
     @Test
-    void testPassingObjectIn() {
-        DataFormatter x = new DataFormatter(testData);
-        String result = x.extractValuesToString();
-        assertEquals("'18725','walk','kris','hardy','test'", result);
+    void testPassingObjectToClass() {
+        TransferTransaction a = new TransferTransaction();
+        DataFormatter c = new DataFormatter(a);
+        ClientAccount b = new ClientAccount();
+        DataFormatter d = new DataFormatter(b);
+        assertEquals("accounts", d.tableName);
+        assertEquals("transactions", c.tableName);
+    }
+
+    @Test
+    void testGettingDataOutOfObject() throws Exception {
+        ClientAccount z = new ClientAccount(testAccountData);
+        DataFormatter y = new DataFormatter(z);
+        String query = "insert into " + y.tableName + " values(" + y.extractValuesToString() + ")";
+        assertEquals("insert into accounts values('123','40-25-99','12345678','308.50','1500','1')", query);
+    }
+
+    @Test
+    void buildWriteQueryFromObject() throws Exception {
+        ClientAccount z = new ClientAccount(testAccountData);
+        DataFormatter y = new DataFormatter(z);
+        String sqlQuery = y.buildWriteQuery();
+        assertEquals("insert into accounts values('123','40-25-99','12345678','308.50','1500','1')", sqlQuery);
     }
 }
