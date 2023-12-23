b78420ecef83c45a3d3ca90ec06f89ddaab630b1
kris.hardy <kristian.hardy@gmail.com>
2023-12-23 17:05:03 +0000

    just the one map needed for this.

--- src/main/java/database/MapFieldsToColumns.java
+++ src/main/java/database/MapFieldsToColumns.java
@@ -5,13 +5,34 @@
 package database;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class MapFieldsToColumns {
 
-    static  Map<String, String> account = new HashMap<>(Map.of("customerID","customer_id", "sortCode","sort_code", "accountNumber","account_number", "currentBalance","current_balance", "overdraftLimit","overdraft_limit", "signatories","signatories"));
+    static HashMap<String,String> mappings = new HashMap<>() {{
+        put("customerID", "customer_id");
+        put("username", "username");
+        put("password","password");
+        put("firstName", "first_name");
+        put("lastName", "last_name");
+        put("dob", "dob");
+        put("address1", "address1");
+        put("address2", "address2");
+        put("addressTown", "town");
+        put("addressPostcode", "postcode");
+        put("accountNumber", "account_number");
+        put("accountType", "account_type");
+        put("overdraftLimit", "overdraft_limit");
+        put("signatories", "signatories");
+        put("transactionID", "transaction_id");
+        put("transactionAmount", "transaction_amount");
+        put("transactionType", "transaction_type");
+        put("previousBalance", "previous_balance");
+        put("newBalance", "new_balance");
+        put("transactionTime", "transaction_time");
+        put("authorised", "authorised");
+        put("additionalInfo", "additional_info");
+    }};
 
-    static Map<String, String> customer = new HashMap<>(Map.of("customerID","customer_id", "firstName","first_name","lastName","last_name", "dob","dob", "address1","address1", "address2","address2", "addressTown","town", "addressPostcode","postcode"));
 
-    static Map<String, String> transaction = new HashMap<>(Map.of("transactionID","transaction_id", "accountNumber","account_number", "transactionAmount","transaction_amount", "transactionType","transaction_type", "previousBalance","previous_balance", "newBalance","new_balance", "transactionTime","transaction_time", "authorised","authorised", "additionalInfo","additional_info"));
 }
+    
