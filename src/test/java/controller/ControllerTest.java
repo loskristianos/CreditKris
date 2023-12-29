abf8d5d1e07632a009ce2afb0fc76b23735e82dd
kris.hardy <kristian.hardy@gmail.com>
2023-12-29 14:43:23 +0000

    write pending transaction

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ControllerTest {
     DataObjectCreator doc = new DataObjectCreator();
     DataHandlerCreator han = new DataHandlerCreator();
-    HashMap<String, String> testDataHM = new HashMap<>(Map.of("transactionID","31826",  "accountNumber","12345678", "transactionAmount","34.65", "transactionType","Deposit", "previousBalance","538.75", "newBalance","573.40", "transactionTime","2023/12/19 14:30:00", "authorised","Y", "additionalInfo",""));
+    HashMap<String, String> testDataHM = new HashMap<>(Map.of("transactionID","31826",  "accountNumber","909912", "transactionAmount","34.65", "transactionType","Deposit", "previousBalance","538.75", "newBalance","573.40", "transactionTime","2023/12/19 14:30:00", "authorised","Y", "additionalInfo",""));
 
 
 
@@ -23,5 +23,12 @@ class ControllerTest {
         Transaction tx = new DepositTransaction(testDataHM);
         Controller x = new Controller(doc, han);
         x.newTransaction(tx);
+    }
+
+    @Test
+    void newPendingRecords() {
+        Transaction tx = new DepositTransaction(testDataHM);
+        Controller y = new Controller(new DataObjectCreator(), new DataHandlerCreator());
+        y.newPendingTransaction(tx);
     }
 }
