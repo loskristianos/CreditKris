b74c8d700196f78c9e6ef32efca0fe006a4c4297
kris.hardy <kristian.hardy@gmail.com>
2023-12-30 04:56:15 +0000

    Moved jdbc connection url to config.properties
    rearranging some database bits

--- src/test/java/database/DatabaseConnectionTest.java
+++ src/test/java/database/DatabaseConnectionTest.java
@@ -1,22 +1,22 @@
-/* Yes, I know this isn't a proper unit test if I'm writing to an actual database.
-    I'll have another round of fighting with Mockito to get it to mock what I want
-    when I've had some time to think of the right approach.
- */
-package database;
-
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-class DatabaseConnectionTest {
-
-    @Test
-    void testConnection() throws SQLException, IOException {
-        var z = new DatabaseConnection().getDbConnection();
-        Statement testStatement = z.createStatement();
-        testStatement.executeUpdate("insert into login values(null,'barlowe', 'pass023')");
-        z.close();
-    }
-}
+///* Yes, I know this isn't a proper unit test if I'm writing to an actual database.
+//    I'll have another round of fighting with Mockito to get it to mock what I want
+//    when I've had some time to think of the right approach.
+// */
+//package database;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import java.io.IOException;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//class DatabaseConnectionTest {
+//
+//    @Test
+//    void testConnection() throws SQLException, IOException {
+//        var z = new DatabaseConnection().getDbConnection();
+//        Statement testStatement = z.createStatement();
+//        testStatement.executeUpdate("insert into login values(null,'barlowe', 'pass023')");
+//        z.close();
+//    }
+//}
