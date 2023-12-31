409939f86252dafd4a94def990ffc497b6c8c838
kris.hardy <kristian.hardy@gmail.com>
2023-12-31 15:01:33 +0000

    new ViewController for UI

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -33,8 +33,8 @@ public class Controller {
 
 
     // customer login, successful login returns Customer object (failed login returns null)
-    public Customer loginAttempt (String username, String password) {
-        LoginObject login = (LoginObject) objectCreator.createLoginObject(username, password);
+    public Customer loginAttempt (DataObject inputObject) {
+        LoginObject login = (LoginObject) inputObject;
         List<DataObject> returnedData = dataHandlerCreator.createLoginDataHandler(login).getRecords();
         if (returnedData.isEmpty()) return null;
         else {
---
+++ src/main/java/controller/ViewController.java
@@ -0,0 +1,36 @@
+/*
+    I am putting this in again for now - I think I'll have all the view classes produce
+    HashMaps from their fields and take HashMaps as inputs to display, and this will handle
+    that in/out process, convert them to the relevant DataObject and invoke the main controller
+    (which probably needs a new name now)
+ */
+package controller;
+
+import customer.Customer;
+import interfaces.*;
+
+
+import java.util.HashMap;
+
+public class ViewController {
+
+    DataHandlerCreator dataHandlerCreator;
+    DataObjectCreator objectCreator;
+    Controller controller = new Controller(objectCreator,dataHandlerCreator);
+
+    public void launchView(String view) {
+        // method to launch the view (either CLI or GUI)
+        // called by main method, input string view controls what view is launched
+
+    }
+
+    public HashMap<String,String> loginView (HashMap<String,String> inputMap) {
+        DataObject login = objectCreator.createLoginObject(inputMap.get("username"),inputMap.get("password"));
+        Customer customer = controller.loginAttempt(login);
+        return customer.getDetails();
+    }
+
+    public HashMap<String,String> customerView (HashMap<String,String> inputMap) {
+return inputMap;
+    }
+}
