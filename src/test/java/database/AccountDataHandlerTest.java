270cf9112734189ef7fdb3fa3eb9bf87d9fecffe
kris.hardy <kristian.hardy@gmail.com>
2024-01-01 23:59:19 +0000

    getRecords() method automated test.
    I hesitate to even call them "unit tests" because they're just horrendous. I mean they work, but urgh. I'll have to find a better way to do this.

--- src/test/java/database/AccountDataHandlerTest.java
+++ src/test/java/database/AccountDataHandlerTest.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,15 +19,14 @@ import static org.mockito.Mockito.when;
 class AccountDataHandlerTest {
 
     @Mock
-    Thread thread;
-    @Mock
     Connection dbConnection;
-//    @Mock
-//    MockedStatic<DriverManager> DriverManager;
+    @Mock ResultSet mockResultSet;
+    @Mock ResultSetMetaData mockMetaData;
     @Mock
     DataObject inputObject;
     @Mock
     Statement statement;
+
     @InjectMocks AccountDataHandler mockAccountDataHandler;
 
     private AutoCloseable x;
@@ -60,7 +58,29 @@ class AccountDataHandlerTest {
     }
 
     @Test
-    void getRecords() {
+    void getRecords() throws SQLException {
+        HashMap<String,String> customerDetails = new HashMap<>(){{
+           put("customerID","123"); put("firstName","Rodney"); put("lastName","Price"); put("dob","12/09/1972");
+           put("address1","1 The Street"); put("address2","Amble"); put("addressTown","MORPETH"); put("addressPostcode","NE65 1BK");
+        }};
+        when(inputObject.getObjectType()).thenReturn("Customer");
+        when(inputObject.getDetails()).thenReturn(customerDetails);
+        try (MockedStatic<DriverManager> mockManager = Mockito.mockStatic(DriverManager.class)) {
+            when(DriverManager.getConnection(anyString())).thenReturn(dbConnection);
+            when(dbConnection.createStatement()).thenReturn(statement);
+            when(statement.executeQuery(anyString())).thenReturn(mockResultSet);
+            when(mockResultSet.next()).thenReturn(true,false);
+            when(mockResultSet.getMetaData()).thenReturn(mockMetaData);
+                when(mockMetaData.getColumnCount()).thenReturn(2);
+                when(mockMetaData.getColumnName(1)).thenReturn("account_number");
+            when(mockResultSet.getString(1)).thenReturn("404616");
+            when(mockMetaData.getColumnName(2)).thenReturn("account_type");
+            when(mockResultSet.getString(2)).thenReturn("Client");
+            List<DataObject> returnedObjects = accountDataHandler.getRecords();
+            verify(statement).executeQuery("SELECT * FROM accounts WHERE account_number IN (SELECT account_number FROM accounts WHERE customer_id = 123 UNION SELECT account_number FROM signatories WHERE customer_id = 123)");
+            assertEquals("Account", returnedObjects.getFirst().getObjectType());
+            assertEquals("404616",returnedObjects.getFirst().getDetails().get("accountNumber"));
+        }
     }
 
     @Test
