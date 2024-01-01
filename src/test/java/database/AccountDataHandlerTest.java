0bc676b2426f77b458f868642640ce3982ddd2a0
kris.hardy <kristian.hardy@gmail.com>
2024-01-01 22:27:21 +0000

    Unit testing for DataHandler class/subclasses without requiring a database connection
    Uses Mockito to simulate data returned from the database to enable testing of the methods by themselves.

--- src/test/java/database/AccountDataHandlerTest.java
+++ src/test/java/database/AccountDataHandlerTest.java
@@ -1,28 +1,69 @@
 package database;
 
-import account.Account;
-import account.ClientAccount;
-import customer.Customer;
+import interfaces.DataObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.HashMap;
-import java.util.Map;
+import org.mockito.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 class AccountDataHandlerTest {
 
-    HashMap<String,String> accountData = new HashMap<>(Map.of("customerID","123", "accountNumber","12345678", "currentBalance","308.50", "overdraftLimit","1500", "signatories","1","accountType","client"));
-    HashMap<String,String> customerData = new HashMap<>(Map.of("customerID","123", "firstName","Mark", "lastName","Myrie", "dob","17/07/1973", "address1","10 The Close", "address2","Southwark", "addressTown","LONDON", "addressPostcode","SE1 1BB"));
-    HashMap<String, String> transactionData = new HashMap<>(Map.of("transactionID","1087",  "accountNumber","12345678", "transactionAmount","34.65", "transactionType","Deposit", "previousBalance","538.75", "newBalance","573.40", "transactionTime","2023/12/19 14:30:00", "authorised","Y", "additionalInfo",""));
+    @Mock
+    Thread thread;
+    @Mock
+    Connection dbConnection;
+//    @Mock
+//    MockedStatic<DriverManager> DriverManager;
+    @Mock
+    DataObject inputObject;
+    @Mock
+    Statement statement;
+    @InjectMocks AccountDataHandler mockAccountDataHandler;
 
+    private AutoCloseable x;
+    AccountDataHandler accountDataHandler;
 
-    @Test
-    void writeAccountData() {
-       Account x = new ClientAccount(accountData);
-       new AccountDataHandler(x).writeNewRecord();
+    @BeforeEach
+    void setUp() {
+        x = MockitoAnnotations.openMocks(this);
+        this.accountDataHandler = mockAccountDataHandler;
     }
 
+    @AfterEach
+    void tearDown() throws Exception{
+        x.close();
+    }
 
+    @Test
+    void writeNewRecord() throws SQLException {
+        HashMap<String,String> inputObjectDetails = new HashMap<>(){{
+            put("accountNumber","1234");put("currentBalance","31.65");put("accountType","Client");
+        }};
+        when(inputObject.getDetails()).thenReturn(inputObjectDetails);
+        try (MockedStatic<DriverManager> mockManager = Mockito.mockStatic(DriverManager.class)) {
+            when(DriverManager.getConnection(anyString())).thenReturn(dbConnection);
+            when(dbConnection.createStatement()).thenReturn(statement);
+            accountDataHandler.writeNewRecord();
+            verify(statement).executeUpdate("INSERT INTO accounts ('account_number','account_type','current_balance') VALUES ('1234','Client','31.65')");
+        }
+    }
 
+    @Test
+    void getRecords() {
+    }
 
+    @Test
+    void update() {
+    }
 }
