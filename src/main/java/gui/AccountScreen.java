fc2e4b5e44159d7a5b5ede367bfd71342e7dbd8a
kris.hardy <kristian.hardy@gmail.com>
2024-01-04 20:50:52 +0000

    AccountScreen - takes list of transactions as a parameter

--- src/main/java/gui/AccountScreen.java
+++ src/main/java/gui/AccountScreen.java
@@ -2,14 +2,25 @@ package gui;
 
 import account.Account;
 import customer.Customer;
+import interfaces.DataObject;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class AccountScreen {
+import java.util.List;
+
+public class AccountScreen extends Application {
     Account account;
     Customer customer;
+    List<DataObject> transactionList;
 
-    public AccountScreen(Account inputAccount, Customer inputCustomer){
+    public AccountScreen(Account inputAccount, Customer inputCustomer, List<DataObject> inputList){
         this.account = inputAccount;
         this.customer = inputCustomer;
+        this.transactionList = inputList;
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
