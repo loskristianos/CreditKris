01499b72fc1673378d3e547bc2d394762af52bb2
kris.hardy <kristian.hardy@gmail.com>
2024-01-04 20:32:40 +0000

    CustomerView - get accounts for Customer object passed to constructor before launching CustomerScreen

--- src/main/java/view/CustomerView.java
+++ src/main/java/view/CustomerView.java
@@ -1,24 +1,28 @@
 package view;
 
+import controller.Controller;
+import customer.Customer;
 import gui.CustomerScreen;
+import interfaces.DataHandlerCreator;
 import interfaces.DataObject;
+import interfaces.DataObjectCreator;
 import javafx.application.Application;
 
 import java.util.List;
 
 public class CustomerView extends View {
-    DataObject customer;
-    List<DataObject> accounts;
+    Controller controller = new Controller(new DataObjectCreator(),new DataHandlerCreator());
+    Customer customer;
+    List<DataObject> accountsList;
 
-    public CustomerView(List<DataObject> inputList, DataObject inputObject) {
-        this.customer = inputObject;
-        this.accounts = inputList;
-        displayView();
+    public CustomerView(DataObject inputCustomer) {
+        this.customer = (Customer) inputCustomer;
+        this.accountsList = controller.getCustomerAccounts(customer);
     }
 
     @Override
     public void displayView() {
-        Application customerScreen = new CustomerScreen(accounts,customer);
+        Application customerScreen = new CustomerScreen(accountsList,customer);
         Application.launch();
     }
 
