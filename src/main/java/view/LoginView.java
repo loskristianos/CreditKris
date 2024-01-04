dd0a58b7e2121659045d7401c27316185cecfad9
kris.hardy <kristian.hardy@gmail.com>
2024-01-04 20:30:57 +0000

    method to send login attempt, return Customer and launch CustomerView with that Customer

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,11 +1,28 @@
 package view;
 
+import controller.Controller;
 import gui.LoginScreen;
+import interfaces.DataHandlerCreator;
+import interfaces.DataObject;
+import interfaces.DataObjectCreator;
+import login.LoginObject;
 
 public class LoginView extends View {
+    Controller controller = new Controller(new DataObjectCreator(), new DataHandlerCreator());
 
     @Override
     public void displayView() {
         LoginScreen.launch();
+    }
+
+    // need some sort of method to listen for submit from LoginScreen? or button calls this method directly?
+    public void loginAttempt(String username, String password){
+        LoginObject loginObject = new DataObjectCreator().createLoginObject(username, password);
+        DataObject returnedCustomer = controller.loginAttempt(loginObject);
+        if (returnedCustomer != null ){
+            new CustomerView(returnedCustomer).displayView();
+        } else {
+            // display error/ launch new customer Screen (to do)
+        }
     }
 }
