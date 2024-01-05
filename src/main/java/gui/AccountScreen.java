71a0c21a95ea6f8f3d4517f9245e8109ab69add8
kris.hardy <kristian.hardy@gmail.com>
2024-01-05 04:22:48 +0000

    AccountScreen - buttons and methods for new transactions

--- src/main/java/gui/AccountScreen.java
+++ src/main/java/gui/AccountScreen.java
@@ -5,14 +5,12 @@ import customer.Customer;
 import interfaces.DataObject;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import transaction.Transaction;
+import view.CreateNewTransactionView;
 
 import java.util.List;
 
@@ -20,11 +18,13 @@ public class AccountScreen {
     Account account;
     Customer customer;
     List<DataObject> transactionList;
+    List<DataObject> accountList;
 
-    public AccountScreen(Account inputAccount, Customer inputCustomer, List<DataObject> inputList){
+    public AccountScreen(Account inputAccount, Customer inputCustomer, List<DataObject> inputTransactionList,List<DataObject> accountList){
         this.account = inputAccount;
         this.customer = inputCustomer;
-        this.transactionList = inputList;
+        this.transactionList = inputTransactionList;
+        this.accountList = accountList;
     }
 
     public void displayScreen(){
@@ -55,6 +55,24 @@ public class AccountScreen {
         accountDetailsPane.add(new Label(currentBalance),1,1,1,1);         // to move further down the pane
         accountDetailsPane.add(new Label("Overdraft Limit"),2,1,1,1);   // to move down
         accountDetailsPane.add(new Label(overdraftLimit),3,1,1,1);         // to move down
+
+        // Initialise some buttons
+        Button newDeposit = new Button ("Deposit");
+        accountDetailsPane.add(newDeposit,0,3,1,1);
+        Button newWithdrawal = new Button("Withdrawal");
+        accountDetailsPane.add(newWithdrawal,1,3,1,1);
+        ComboBox<String> transferAccounts = new ComboBox<>();
+        for (DataObject object : accountList) {
+            Account x = (Account) object;
+            if (x.getAccountNumber() != accountNumber)
+                transferAccounts.getItems().add(x.getAccountNumber());
+        }
+        Button newTransfer = new Button("Transfer");
+        if (accountList.size()>1){
+            accountDetailsPane.add(newTransfer,2,3,1,1);
+         //   accountDetailsPane.add(transferAccounts,3,3,1,1);
+        }
+
 
         // initialise a TableView
         TableView<Transaction> transactionTableView = new TableView<>();
@@ -98,5 +116,23 @@ public class AccountScreen {
         stage.setScene(scene);
         stage.setTitle("Viewing details for " + accountType + " account number" + accountNumber);
         stage.show();
+
+        // action on transaction buttons
+        newDeposit.setOnAction(actionEvent -> {
+            new CreateNewTransactionView(account,customer,"Deposit").displayView();
+        });
+
+        newWithdrawal.setOnAction((actionEvent -> {
+            new CreateNewTransactionView(account,customer,"Withdrawal").displayView();
+        }));
+
+        newTransfer.setOnAction(actionEvent -> {
+            accountDetailsPane.add(new Label("Select an account to transfer to"),3,3,1,1);
+            accountDetailsPane.add(transferAccounts, 4, 3, 1, 1);
+            transferAccounts.setOnAction((actionEvent1 -> {
+                String selectedAccount = transferAccounts.getSelectionModel().getSelectedItem();
+                new CreateNewTransactionView(account, customer, "Transfer", selectedAccount).displayView();
+            }));
+        });
     }
 }
