30fa46d4f168f718ff94c1229dcd54ccded1ef26
kris.hardy <kristian.hardy@gmail.com>
2024-01-05 04:23:23 +0000

    AccountView - changes for new transactions

--- src/main/java/view/AccountView.java
+++ src/main/java/view/AccountView.java
@@ -16,15 +16,17 @@ public class AccountView extends View{
     Controller controller = new Controller(new DataObjectCreator(),new DataHandlerCreator());
     Account account;
     Customer customer;
+    List<DataObject> accountList;
     List<DataObject> transactionList;
 
-    public AccountView(Account inputAccount, Customer inputCustomer){
+    public AccountView(Account inputAccount, Customer inputCustomer, List<DataObject> accountList){
         this.account = inputAccount;
         this.customer = inputCustomer;
-        transactionList = controller.getAccountTransactions(account);
+        this.accountList = accountList;
+        this.transactionList = controller.getAccountTransactions(account);
     }
     @Override
     public void displayView() {
-        new AccountScreen(account,customer,transactionList).displayScreen();
+        new AccountScreen(account,customer,transactionList,accountList).displayScreen();
     }
 }
