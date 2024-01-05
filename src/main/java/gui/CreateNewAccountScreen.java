8695687492d581b9ab7069f382530fc33adee40e
kris.hardy <kristian.hardy@gmail.com>
2024-01-05 04:24:25 +0000

    CreateNewAccountScreen outline

---
+++ src/main/java/gui/CreateNewAccountScreen.java
@@ -0,0 +1,22 @@
+package gui;
+
+import customer.Customer;
+import javafx.scene.layout.GridPane;
+
+public class CreateNewAccountScreen {
+    Customer customer;
+
+    public CreateNewAccountScreen(Customer inputCustomer) {
+        this.customer = inputCustomer;
+    }
+
+    public void displayScreen(){
+        start();
+    }
+
+    public void start(){
+        // only needs account type selection and a way to input signatories if required
+        // - everything else is set automatically.
+
+    }
+}
