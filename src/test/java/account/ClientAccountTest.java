f0f1654fee263151950f4d5043d0c2e9dfdc488e
kris.hardy <kristian.hardy@gmail.com>
2024-01-07 15:54:26 +0000

    Account Tests - removed no-args constructor

--- src/test/java/account/ClientAccountTest.java
+++ src/test/java/account/ClientAccountTest.java
@@ -9,32 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ClientAccountTest {
 
-    Map<String,String> nullDataHM = new HashMap<>() {{
-        put("customerID",null); put("accountNumber",null); put("currentBalance",null); put("overdraftLimit",null); put("signatories",null); put("accountType",null);
-    }};
     HashMap<String,String> testDataHM = new HashMap<>(Map.of("customerID","123", "accountNumber","12345678", "currentBalance","308.50", "overdraftLimit","500", "signatories","1"));
     HashMap<String,String> expectedDataHM = new HashMap<>(Map.of("customerID","123",  "accountNumber","12345678", "currentBalance","308.50", "overdraftLimit","1500", "signatories","1", "accountType","Client"));
 
 
-    @Test
-    void noArgsConstructorHM() {
-        Account account = new ClientAccount();
-        HashMap<String,String> returnedDetails = account.getDetails();
-        nullDataHM.put("accountType","Client");nullDataHM.put("overdraftLimit","1500");
-        assertEquals(nullDataHM, returnedDetails);
-    }
-
-    @Test
-    void noArgsConstructorSetDetailsHM() {
-        Account account = new ClientAccount();
-        account.setDetails(testDataHM);
-        HashMap<String, String> returnedData = account.getDetails();
-        assertEquals(expectedDataHM, returnedData);
-    }
-    @Test
+        @Test
     void withArgsConstructorHM() {
         Account account = new ClientAccount(testDataHM);
-    //    expectedDataHM.put("overdraftLimit","1500");
         HashMap<String, String> returnedDetails = account.getDetails();
         assertEquals(expectedDataHM, returnedDetails);
     }
@@ -43,12 +24,9 @@ class ClientAccountTest {
     void withArgsConstructorSetBalanceHM() {
         Account account = new ClientAccount(testDataHM);
         account.setCurrentBalance("907.56");
-    //    expectedDataHM.put("overdraftLimit","1500");
+
         expectedDataHM.put("currentBalance","907.56");
         HashMap<String,String> returnedDetails = account.getDetails();
         assertEquals(expectedDataHM, returnedDetails);
     }
-
-
-
 }
