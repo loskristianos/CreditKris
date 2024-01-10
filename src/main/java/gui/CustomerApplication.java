3dc186bf36cdb8b372c1ae7a9559093730546ee0
kris.hardy <kristian.hardy@gmail.com>
2024-01-10 21:41:31 +0000

    CustomerApplication - added field previousScene and methods to set it

--- src/main/java/gui/CustomerApplication.java
+++ src/main/java/gui/CustomerApplication.java
@@ -9,17 +9,20 @@ import javafx.stage.Stage;
 public class CustomerApplication extends Application {
     Customer customer;
     CustomerController customerController;
-    Stage stage;
+    Scene previousScene;
 
     public CustomerApplication(Customer customer){
         this.customer = customer;
         this.customerController = new CustomerController();
         customerController.setCustomer(customer);
     }
-
+    public void setPreviousScene(Scene scene) {
+        previousScene = scene;
+    }
     @Override
     public void start(Stage stage) throws Exception {
         customerController.setCurrentStage(stage);
+        customerController.setPreviousScene(previousScene);
         FXMLLoader fxmlloader =new FXMLLoader();
         fxmlloader.setController(customerController);
         fxmlloader.setLocation(getClass().getResource("customer-view.fxml"));
