216319a15d335e2671b3353063b4f83c9284fde1
kris.hardy <kristian.hardy@gmail.com>
2024-01-10 21:53:48 +0000

    CustomerApplication - removed previousScene field/method, set customerController.previousScene to this.scene
    we don't need to go back to previous scene from here, we just log out. customerController wants the current scene as its previous scene so we can come back here from the account/transaction screens.

--- src/main/java/gui/CustomerApplication.java
+++ src/main/java/gui/CustomerApplication.java
@@ -9,24 +9,21 @@ import javafx.stage.Stage;
 public class CustomerApplication extends Application {
     Customer customer;
     CustomerController customerController;
-    Scene previousScene;
 
     public CustomerApplication(Customer customer){
         this.customer = customer;
         this.customerController = new CustomerController();
         customerController.setCustomer(customer);
     }
-    public void setPreviousScene(Scene scene) {
-        previousScene = scene;
-    }
+
     @Override
     public void start(Stage stage) throws Exception {
         customerController.setCurrentStage(stage);
-        customerController.setPreviousScene(previousScene);
         FXMLLoader fxmlloader =new FXMLLoader();
         fxmlloader.setController(customerController);
         fxmlloader.setLocation(getClass().getResource("customer-view.fxml"));
         Scene scene = new Scene(fxmlloader.load());
+        customerController.setPreviousScene(scene);
         stage.setTitle("Customer Details");
         stage.setScene(scene);
     }
