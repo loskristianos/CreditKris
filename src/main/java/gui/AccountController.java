9c93c48f749015a5fa255136b01f4938e922f776
kris.hardy <kristian.hardy@gmail.com>
2024-01-10 14:41:03 +0000

    new AccountController (still needs a couple of ButtonActions

---
+++ src/main/java/gui/AccountController.java
@@ -0,0 +1,93 @@
+package gui;
+
+import account.Account;
+import customer.Customer;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import transaction.Transaction;
+
+import java.util.List;
+
+public class AccountController {
+    Stage currentStage;
+    Account account;
+    Customer customer;
+    List<Transaction> transactionList;
+    List<Account> accountList;
+
+    @FXML private TextField accountNumberDisplay;
+    @FXML private TextField accountTypeDisplay;
+    @FXML private TextField currentBalanceDisplay;
+    @FXML private TextField overdraftLimitDisplay;
+    @FXML private Button transferButton;
+    @FXML private ComboBox<Account> transferAccounts;
+    @FXML private TableView<Transaction> transactionTableView;
+    @FXML private TableColumn<Transaction,String> transactionTimeColumn;
+    @FXML private TableColumn<Transaction,String> transactionTypeColumn;
+    @FXML private TableColumn<Transaction,String> transactionAmountColumn;
+    @FXML private TableColumn<Transaction,String> previousBalanceColumn;
+    @FXML private TableColumn<Transaction,String> newBalanceColumn;
+    @FXML private TableColumn<Transaction,String> transactionIdColumn;
+
+    public AccountController(){}
+
+    @FXML private void initialize(){
+        accountNumberDisplay.setText(account.getAccountNumber());
+        accountTypeDisplay.setText(account.getAccountType());
+        currentBalanceDisplay.setText(account.getCurrentBalance());
+        overdraftLimitDisplay.setText(account.getOverdraftLimit());
+        if(accountList.size()>1) transferButton.setVisible(true);
+        for (Account account : accountList){
+            if (!account.getAccountNumber().equals(this.account.getAccountNumber()))
+                transferAccounts.getItems().add(account);
+        }
+
+        transactionTimeColumn.setCellValueFactory(new PropertyValueFactory<>("transactionTime"));
+        transactionTypeColumn.setCellValueFactory(new PropertyValueFactory<>("transactionType"));
+        transactionAmountColumn.setCellValueFactory(new PropertyValueFactory<>("transactionAmount"));
+        previousBalanceColumn.setCellValueFactory(new PropertyValueFactory<>("previousBalance"));
+        newBalanceColumn.setCellValueFactory(new PropertyValueFactory<>("newBalance"));
+        transactionIdColumn.setCellValueFactory(new PropertyValueFactory<>("transactionID"));
+
+        for (Transaction transaction : transactionList) {
+            transactionTableView.getItems().add(transaction);
+        }
+    }
+
+
+    public void setCurrentStage(Stage stage){
+        currentStage = stage;
+    }
+    public void setAccount(Account account){
+        this.account = account;
+    }
+    public void setCustomer(Customer customer){
+        this.customer = customer;
+    }
+    public void setTransactionList(List<Transaction> transactionList){
+        this.transactionList = transactionList;
+    }
+    public void setAccountList(List<Account> accountList){
+        this.accountList = accountList;
+    }
+
+    @FXML private void depositButtonAction(){
+        // launch newTransaction Dialogue
+    }
+
+    @FXML private void withdrawalButtonAction(){
+        // launch newTransactionDialogue
+    }
+
+    @FXML private void transferButtonAction(){
+        transferAccounts.setVisible(true);
+    }
+
+    @FXML private void transferAccountAction(){
+        // launch newTransactionDialogue
+    }
+
+}
+
