73dfd57fe7e22afc195d2ffcb12b562815dfc3fb
kris.hardy <kristian.hardy@gmail.com>
2024-01-11 13:14:47 +0000

    TransactionDAO - new method getThosTransaction

--- src/main/java/dao/TransactionDAO.java
+++ src/main/java/dao/TransactionDAO.java
@@ -8,9 +8,11 @@ import java.util.HashMap;
 import java.util.List;
 
 public class TransactionDAO extends DAO{
+    Transaction transaction;
     Account account;
     public TransactionDAO(Transaction inputTransaction) {
         super(inputTransaction);
+        this.transaction = inputTransaction;
     }
 
     public TransactionDAO(Account account) {
@@ -39,5 +41,17 @@ public class TransactionDAO extends DAO{
             transactionList.add(returnedTransaction);
         }
         return transactionList;
+    }
+
+    public Transaction getThisTransaction(){
+        String transactionID = transaction.getTransactionID();
+        sqlStatement = "SELECT * FROM transactions WHERE transaction_id = '" + transactionID + "'";
+        HashMap<String,String> returnMap = super.databaseLookup().getFirst();
+        return switch(returnMap.get("transactionType")){
+            case "Deposit": yield new DepositTransaction(returnMap);
+            case "Withdrawal": yield new WithdrawalTransaction(returnMap);
+            case "Transfer In","Transfer Out": yield new TransferTransaction(returnMap);
+            default:yield null;
+        };
     }
 }
