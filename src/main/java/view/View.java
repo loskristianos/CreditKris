3b5b2a6a137465bfcc2b760ef3883fa7bd7b4682
kris.hardy <kristian.hardy@gmail.com>
2024-01-13 22:26:13 +0000

    Deleted the old Screen and View files from the first attempt at a gui

--- src/main/java/view/View.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-public abstract class View {
-
-    public abstract void displayView();
-        /* for javafx create/display the stage, for ConsoleUI build the prompts etc
-            returns this.view.View(); */
-}
