a5187eb825d4c019e32ecb4bb12db48d5dc7ff81
kris.hardy <kristian.hardy@gmail.com>
2024-01-14 21:11:43 +0000

    Call Setup.runChecks before launching the Login gui

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import database.Setup;
 import gui.LoginApplication;
 
 public class Main {
 
     public static void main(String[] args){
+
+        Setup.runChecks();
         new LoginApplication().displayScreen();
     }
 }
