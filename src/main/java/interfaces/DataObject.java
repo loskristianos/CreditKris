67e1ed3026ca4d452e17e5603ed05e744badf791
kris.hardy <kristian.hardy@gmail.com>
2024-01-14 21:33:41 +0000

    DataObject - comment added (mainly about why this interface still exists)

--- src/main/java/interfaces/DataObject.java
+++ src/main/java/interfaces/DataObject.java
@@ -1,3 +1,11 @@
+/*
+    This interface is really a remnant from an earlier design implementation which is largely not used
+    anywhere anymore, with the MASSIVE EXCEPTION of the DAO superclass which still uses it in the construction
+    of Insert statements for all the DataObject implementations (so the same method can produce insert
+    statements for Customer, Account, Transaction, Signatory, PendingTransaction and Login objects). So until
+    I can rewrite that into something better this needs to stay.
+ */
+
 package interfaces;
 
 import java.util.HashMap;
