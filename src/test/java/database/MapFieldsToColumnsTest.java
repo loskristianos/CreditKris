5e7fdedf14bd61687254e7151a4aa4c7423d3e22
kris.hardy <kristian.hardy@gmail.com>
2024-01-15 12:36:29 +0000

    updated MapFieldsToCoilumns unit tests

--- src/test/java/database/MapFieldsToColumnsTest.java
+++ src/test/java/database/MapFieldsToColumnsTest.java
@@ -2,6 +2,12 @@ package database;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static database.MapFieldsToColumns.mappingsFromDB;
+import static database.MapFieldsToColumns.mappingsToDB;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapFieldsToColumnsTest {
@@ -9,14 +15,27 @@ class MapFieldsToColumnsTest {
     @Test
     void mappingsObjectToDB() {
         String objectField = "customerID";
-        String databaseField = MapFieldsToColumns.mappingsToDB.get(objectField);
+        String databaseField = mappingsToDB.get(objectField);
         assertEquals("customer_id", databaseField);
     }
 
     @Test
     void mappingsDBtoObject() {
         String returnedDatabaseColumn = "current_balance";
-        String objectField = MapFieldsToColumns.mappingsFromDB.get(returnedDatabaseColumn);
+        String objectField = mappingsFromDB.get(returnedDatabaseColumn);
         assertEquals("currentBalance", objectField);
     }
+
+    @Test
+    void mapsAreTheSame() {
+        // compare the list of keys from one map to the list of values from the other, and vice versa
+        List<String> mapToDBKeys = new ArrayList<>(mappingsToDB.keySet().stream().toList()); Collections.sort(mapToDBKeys);
+        List<String> mapFromDBValues = new ArrayList<>(mappingsFromDB.values().stream().toList()); Collections.sort(mapFromDBValues);
+        assertEquals(mapToDBKeys, mapFromDBValues);
+        List<String> mapFromDBKeys = new ArrayList<>(mappingsFromDB.keySet().stream().toList()); Collections.sort(mapFromDBKeys);
+        List<String> mapToDBValues = new ArrayList<>(mappingsToDB.values().stream().toList()); Collections.sort(mapToDBValues);
+        assertEquals(mapFromDBKeys,mapToDBValues);
+    }
+
+
 }
