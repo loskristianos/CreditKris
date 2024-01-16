8789b2561ac69a689b2cf3be373a45be14bb835d
kris.hardy <kristian.hardy@gmail.com>
2024-01-16 14:23:50 +0000

    New Account and Transaction Classes. These will replace abstract Account and Transaction superclasses and all their subclasses.
    And then I'll have to redo everything else, but it should be a whole lot neater in the end (especially anywhere we're creating a new account/transaction like on database lookup returns, where we'll not have a load of switches to determine what type of object to create)

---
+++ src/main/java/newobjects/Account.java
@@ -0,0 +1,125 @@
+/*
+    new implementation of Account objects - instead of having a load of subclasses with only minor
+    differences (type, overdraftLimit) just have the one and set them on creation.
+ */
+package newobjects;
+
+import customer.Customer;
+
+import java.math.BigDecimal;
+
+public class Account {
+    public enum Type {CLIENT, BUSINESS, COMMUNITY}
+    private Customer customer;
+    private String accountNumber;
+    private Type accountType;
+    private BigDecimal currentBalance;
+    private BigDecimal overdraftLimit;
+    private Integer signatories;
+
+    public Account(Type type){
+        setAccountType(type);
+        setOverdraftLimit(type);
+    }
+
+    public Account(Transaction transaction){
+        // apply transaction to account
+    }
+
+    public Account(Customer customer){
+        this.customer = customer;
+    }
+
+    // set fields
+    public void setCurrentBalance(BigDecimal currentBalance){
+        this.currentBalance = currentBalance;
+    }
+    private void setOverdraftLimit(Type type){
+        switch (type){
+            case CLIENT -> overdraftLimit = new BigDecimal("1500.00");
+            case BUSINESS -> overdraftLimit = new BigDecimal("1000.00");
+            case COMMUNITY -> overdraftLimit = new BigDecimal("2500.00");
+        }
+    }
+    public void setSignatories(Integer signatories){
+        this.signatories = signatories;
+    }
+    private void setAccountType(Type accountType){
+        this.accountType = accountType;
+    }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    // get fields
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public String getAccountType(){
+        return String.valueOf(accountType);
+    }
+    public BigDecimal getCurrentBalance() {
+        return currentBalance;
+    }
+    public BigDecimal getOverdraftLimit() {
+        return overdraftLimit;
+    }
+    public BigDecimal getAvailableBalance(){
+        return currentBalance.add(overdraftLimit);
+    }
+    public Integer getSignatories() {
+        return signatories;
+    }
+
+    // Transaction methods
+    public Transaction validateTransaction(Transaction transaction){
+       if (signatoryCheck(transaction)==-1) return transaction;
+       else if (balanceCheck(transaction)==-1) return transaction;
+       else {
+           applyTransaction(transaction);
+           return transaction;
+       }
+    }
+
+    private void applyTransaction(Transaction transaction){
+        switch(transaction.getTransactionType()){
+            case DEPOSIT, TRANSFER_IN -> paymentIn(transaction);
+            case WITHDRAWAL, TRANSFER_OUT -> paymentOut(transaction);
+        }
+    }
+    private void paymentIn(Transaction transaction){
+        BigDecimal transactionAmount = transaction.getTransactionAmount();
+        transaction.setPreviousBalance(currentBalance);
+        setCurrentBalance(currentBalance.add(transactionAmount));
+        transaction.setNewBalance(currentBalance);
+        transaction.setTransactionStatus(Transaction.Status.COMPLETE);
+    }
+    private void paymentOut(Transaction transaction){
+        BigDecimal transactionAmount = transaction.getTransactionAmount();
+        transaction.setPreviousBalance(currentBalance);
+        setCurrentBalance(currentBalance.subtract(transactionAmount));
+        transaction.setNewBalance(currentBalance);
+        transaction.setTransactionStatus(Transaction.Status.COMPLETE);
+    }
+    private int balanceCheck(Transaction transaction) {
+        BigDecimal transactionAmount = transaction.getTransactionAmount();
+        switch (transaction.getTransactionType()) {
+            case WITHDRAWAL, TRANSFER_OUT -> {
+                if (transactionAmount.compareTo(getAvailableBalance()) > 0) {
+                    transaction.setTransactionStatus(Transaction.Status.FAILED_BALANCE);
+                    return -1;
+                } else return 0;
+            }
+            case DEPOSIT, TRANSFER_IN -> {return 0;}
+            default -> {return 0;}
+        }
+    }
+    private int signatoryCheck(Transaction transaction) {
+        Transaction.Status status = transaction.getTransactionStatus();
+        if (getSignatories()==0 || status.equals(Transaction.Status.AUTHORISED)) return 0;
+        else {
+            transaction.setTransactionStatus(Transaction.Status.REQUIRES_AUTHORISATION);
+            return -1;
+        }
+    }
+}
---
+++ src/main/java/newobjects/Transaction.java
@@ -0,0 +1,104 @@
+package newobjects;
+
+import java.math.BigDecimal;
+
+public class Transaction {
+    public enum Type {DEPOSIT, WITHDRAWAL, TRANSFER_IN, TRANSFER_OUT}
+    public enum Status {COMPLETE,FAILED_BALANCE,AUTHORISED, REQUIRES_AUTHORISATION}
+    private String transactionID;
+    private String accountNumber;
+    private String customerID;          // customerID of the user requesting the transaction (only really applicable to accounts with more than one signatory)
+    private BigDecimal transactionAmount;
+    private Type transactionType;
+    private BigDecimal previousBalance;
+    private BigDecimal newBalance;
+    private String transactionTime;
+    private Status transactionStatus;
+    private String transferAccountNumber; // only used for TransferTransaction
+
+    public Transaction(){
+        // set TransactionType here
+    }
+
+    // set fields
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setTransactionID(String transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public void setTransactionAmount(BigDecimal transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public void setPreviousBalance(BigDecimal previousBalance) {
+        this.previousBalance = previousBalance;
+    }
+
+    public void setNewBalance(BigDecimal newBalance) {
+        this.newBalance = newBalance;
+    }
+
+    public void setTransferAccountNumber(String transferAccountNumber) {
+        this.transferAccountNumber = transferAccountNumber;
+    }
+
+    private void setTransactionType(Type transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+
+    public void setTransactionStatus(Status transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+    // get fields
+
+    public String getTransactionID() {
+        return transactionID;
+    }
+
+    public Type getTransactionType() {
+        return transactionType;
+    }
+
+    public BigDecimal getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public BigDecimal getNewBalance() {
+        return newBalance;
+    }
+
+    public BigDecimal getPreviousBalance() {
+        return previousBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getTransactionTime() {
+        return transactionTime;
+    }
+
+    public String getTransferAccountNumber() {
+        return transferAccountNumber;
+    }
+
+    public Status getTransactionStatus(){
+        return transactionStatus;
+    }
+}
