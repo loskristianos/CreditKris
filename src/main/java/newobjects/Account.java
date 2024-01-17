26d2513cbed2fb75642afc3e1579dcbeed058c98
kris.hardy <kristian.hardy@gmail.com>
2024-01-17 13:01:58 +0000

    Account - removed constructors with transaction and customer (for now, until we need them)

--- src/main/java/newobjects/Account.java
+++ src/main/java/newobjects/Account.java
@@ -23,14 +23,6 @@ public class Account {
         setOverdraftLimit(type);
     }
 
-    public Account(Transaction transaction){
-        this.transaction = transaction;
-    }
-
-    public Account(Customer customer){
-        this.customer = customer;
-    }
-
     // set fields
     public void setCurrentBalance(BigDecimal currentBalance){
         this.currentBalance = currentBalance;
