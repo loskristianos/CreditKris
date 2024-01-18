53abc5ce2e8769d139603300ef5c1bd92062640c
kris.hardy <kristian.hardy@gmail.com>
2024-01-18 10:49:48 +0000

    Login get/set methods

--- src/main/java/newobjects/Login.java
+++ src/main/java/newobjects/Login.java
@@ -15,6 +15,22 @@ public class Login{
     }
 
     public void setPassword (String password){
-        this.password = }
+        this.password = password;
+    }
 
+    public void setCustomerID(Integer customerID){
+        this.customerID = customerID;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public Integer getCustomerID(){
+        return customerID;
+    }
 }
