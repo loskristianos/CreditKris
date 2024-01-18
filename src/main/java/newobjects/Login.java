8ee5baf170ac9e1071ae93e82ec8bd32cbb6afee
kris.hardy <kristian.hardy@gmail.com>
2024-01-18 10:09:11 +0000

    start of new Login class

---
+++ src/main/java/newobjects/Login.java
@@ -0,0 +1,20 @@
+package newobjects;
+
+public class Login{
+
+    public String username;
+    public String password;
+    public Integer customerID;
+
+
+    public Login(){
+    }
+    
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword (String password){
+        this.password = }
+
+}
