3a966983d8ff20c2df8a96d0a2f851b7d4c8b126
kris.hardy <kristian.hardy@gmail.com>
2024-01-19 21:42:58 +0000

    new Signatory class

---
+++ src/main/java/newobjects/Signatory.java
@@ -0,0 +1,38 @@
+package newobjects;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "signatories")
+public class Signatory {
+
+    @DatabaseField
+    private Integer customerID;
+    @DatabaseField
+    private Integer accountNumber;
+
+    private Signatory(Integer customerID, Integer accountNumber){
+        setCustomerID(customerID);
+        setAccountNumber(accountNumber);
+    }
+
+    public Signatory createSignatory(Integer customerID, Integer AccountNumber){
+        return new Signatory(customerID, accountNumber);
+    }
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Integer customerID) {
+        this.customerID = customerID;
+    }
+}
