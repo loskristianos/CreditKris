f3968f002d36e37b3babd992a11d862e0361a04b
kris.hardy <kristian.hardy@gmail.com>
2024-01-20 01:18:55 +0000

    Deleted Dao/DaoImpl classes

--- src/main/java/newdao/AccountDao.java
+++
@@ -1,7 +0,0 @@
-package newdao;
-
-import com.j256.ormlite.dao.Dao;
-import newobjects.Account;
-
-public interface AccountDao extends Dao<Account, Integer> {
-}
--- src/main/java/newdao/TransactionDao.java
+++
@@ -1,7 +0,0 @@
-package newdao;
-
-import com.j256.ormlite.dao.Dao;
-import newobjects.Transaction;
-
-public interface TransactionDao extends Dao<Transaction, Integer> {
-}
